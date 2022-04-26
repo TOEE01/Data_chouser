9be8985ff256f1d18ada37a8a0b660094edaaa8d
TOEE01 <69540536+TOEE01@users.noreply.github.com>
2022-04-25 22:25:14 -0600

    datoss y progrmas

---
+++ UsodeDataChooser/src/main/java/DAO/Casting.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+/**
+ *
+ * @author recin
+ */
+public class Casting {
+   public void CastingInt(){
+       
+       double d = 79.9;
+       
+       float f = (float)d;
+       
+       System.out.println(d);
+       
+   }
+    
+    
+}
---
+++ UsodeDataChooser/src/main/java/DAO/DataChooser.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.text.SimpleDateFormat;
+
+
+/**
+ *
+ * @author recin
+ */
+public class DataChooser {
+//    public class Funciones{
+//        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
+//        
+//        public String getFecha(JDateChooser jd){
+//            if(jd.getDate() !=null){
+//                return formato.format(jd.getDate);
+//            }else{
+//                return null;
+//            }
+//        }
+    //}
+}
